807273138ae965522e82bfad5c05c4626be17dce
Simo-univpm <92679146+Simo-univpm@users.noreply.github.com>
2023-09-22 22:42:31 +0200

    Delete ProEvolutionPaperSoccerInternational_20_final directory

--- ProEvolutionPaperSoccerInternational_20_final/src/it/unicam/cs/pa/paperSoccer/model/ruleSet/checkEnd.java
+++
@@ -1,9 +0,0 @@
-package it.unicam.cs.pa.paperSoccer.model.ruleSet;
-
-public interface checkEnd {
-
-    boolean score();
-    boolean noMovesAvailable(int currentPlayer);
-    boolean isEnd(int currentPlayer);
-
-}
